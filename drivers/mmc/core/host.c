e47921505d896e65e8e1b9e9a928274d1f475ac1
wwd <ericrock@foxmail.com>
2017-04-11 16:48:26 +0800

    ARM: dts: sun8i-h3-nanopi: Disable mmc1 as boot_device

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -362,7 +362,7 @@ struct mmc_host *mmc_alloc_host(int extra, struct device *dev)
 			err = ida_simple_get(&mmc_host_ida, 1, 0, GFP_KERNEL);
 		}
 	} else {
-		err = ida_simple_get(&mmc_host_ida, 0, 0, GFP_KERNEL);
+		err = ida_simple_get(&mmc_host_ida, 2, 0, GFP_KERNEL);
 	}
 	if (err < 0) {
 		kfree(host);
