d4940f17b1cd5dfded32c87d01bf7ac2cb1d26f7
wwd <ericrock@foxmail.com>
2017-04-12 17:02:45 +0800

    ARM: sunxi: Compatible to allwinner 3.4 kernel

--- drivers/thermal/sun8i_ths.c
+++ drivers/thermal/sun8i_ths.c
@@ -70,7 +70,8 @@ static int sun8i_ths_get_temp(void *_data, int *out)
 		return -EBUSY;
 
 	/* Formula and parameters from the Allwinner 3.4 kernel */
-	*out = 217000 - (int)((data->temp * 1000000) / 8253);
+	/* Compatible with Allwinner Linux 3.4 */
+	*out = (217000 - (int)((data->temp * 1000000) / 8253)) / 1000;
 	return 0;
 }
 
